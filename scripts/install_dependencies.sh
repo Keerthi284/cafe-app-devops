b6a1b0a7215dfbe00e00d74f28a80fe4fdd91dce
Keerthi284 <229y1a04g6@ksrmce.ac.in>
2025-06-28 10:19:36 +0000

    Update deployment scripts

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+cd /home/ec2-user/cafe-app-devops
+npm install
+#!/bin/bash
+cd /home/ec2-user/cafe-app-devops
+npm install
+#!/bin/bash
 sudo yum update -y
 sudo yum install -y nodejs
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 cd /home/ec2-user/cafe-app-devops
-npm install
-node app.js > app.log 2>&1 &
+node app.js > app.out.log 2> app.err.log < /dev/null &
+
+
+
+
